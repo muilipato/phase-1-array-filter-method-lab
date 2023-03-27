a3a0e69710d8c7b14dd48f032175ef99db079d8b
Patrick Maweu <60652548+muilipato@users.noreply.github.com>
2023-03-27 12:21:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const filtered = drivers.filter(findMatching)
+
+function findMatching(drivers, name){
+let filtered = drivers.filter(drivers=>drivers===name||drivers===name.toLowerCase());
+return filtered
+}
+ 
+const transporters = [];
+
+function fuzzyMatch(transporters, prsn){
+    let fltrd= transporters.filter(transporter=>
+         transporter.startsWith(prsn));
+         return fltrd
+    }
+
+const transp = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function  matchName(transp,person){
+  let found = transp.filter(function(trans) {
+    return trans.name == person; });
+return found
+  }
